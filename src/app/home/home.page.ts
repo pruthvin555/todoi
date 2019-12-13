bde04ff101112a412395c57552e32c637d1489fe
pruthvin555 <36711458+pruthvin555@users.noreply.github.com>
2019-12-13 00:37:31 -0600

    Commit Includes - Add, Delete, Complete, filter list.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonList } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss']
 })
 export class HomePage {
+	items = [
+		{ status: 'pending', value: 'apple' },
+		{ status: 'pending', value: 'banana' },
+		{ status: 'pending', value: 'cherry' }
+	];
+	filterItems = [];
+	txtInput = '';
+	filterBy = 'all';
 
-  constructor() {}
+	ionViewDidEnter() {
+		this.filterItems = this.items;
+	}
 
+	AddToDo(event: any) {
+		if (!!this.txtInput) {
+			this.items.push({ status: 'pending', value: this.txtInput });
+			this.txtInput = '';
+			console.log(event);
+		}
+	}
+
+	doFilterStatus(event) {
+		const selectedStatus = event.detail.value;
+		if (selectedStatus == 'all') {
+			this.filterItems = this.items;
+		} else {
+			this.filterItems = this.items.filter(item => item.status == selectedStatus);
+		}
+	}
+
+	doComplete(item) {
+		item.status = 'complete';
+		console.log('do complete');
+	}
+
+	delete(i) {
+		console.log('my delete works');
+		this.items.splice(i, 1);
+	}
 }
